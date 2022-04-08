9366bc1570c6ef83982e2b31e817a4e31bc0a848
Ali Can Tozlu <alicantozlu@gmail.com>
2022-04-08 18:35:25 +0300

    code correction

--- Sources/SwiftHelpers/SwiftHelperExtensions/UIButton.swift
+++ Sources/SwiftHelpers/SwiftHelperExtensions/UIButton.swift
@@ -8,10 +8,5 @@
 import UIKit
 
 extension UIButton{
-    /// Default: cornerRadius: 10 | borderWidth: 0 | borderColor: Black
-    public func cornerConfig(cornerRadius: Double, borderWidth: Double, borderColor: CGColor){
-        self.layer.cornerRadius = cornerRadius
-        self.layer.borderWidth = borderWidth
-        self.layer.borderColor = borderColor
-    }
+
 }
